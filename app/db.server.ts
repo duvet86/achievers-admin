e3bac2fa4f8e9a60e23aad41c2909555964b5d2f
Luca <duvet86@gmail.com>
2023-02-15 18:37:06 +0800

    Working

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-// import { writeFileSync } from "fs";
-// import invariant from "tiny-invariant";
+import { writeFileSync } from "fs";
+import invariant from "tiny-invariant";
 
 let prisma: PrismaClient;
 
@@ -13,24 +13,27 @@ declare global {
 // create a new connection to the DB with every change either.
 // in production we'll have a single connection to the DB.
 if (process.env.NODE_ENV === "production") {
-  // invariant(process.env.DATABASE_URL, "DATABASE_URL must be set");
-  // invariant(process.env.CERT, "CERT must be set");
+  if (process.env.ENABLE_SSL) {
+    invariant(process.env.DATABASE_URL, "DATABASE_URL must be set");
+    invariant(process.env.CERT, "CERT must be set");
 
-  // const dbUrl = process.env.DATABASE_URL.split("?");
+    const dbUrl = process.env.DATABASE_URL.split("?");
 
-  // writeFileSync(
-  //   "/tmp/DigiCertGlobalRootCA.crt.pem",
-  //   Buffer.from(process.env.CERT, "base64")
-  // );
+    writeFileSync(
+      "/tmp/DigiCertGlobalRootCA.crt.pem",
+      Buffer.from(process.env.CERT, "base64")
+    );
 
-  // prisma = new PrismaClient({
-  //   datasources: {
-  //     db: {
-  //       url: dbUrl[0] + "?sslcert=/tmp/DigiCertGlobalRootCA.crt.pem",
-  //     },
-  //   },
-  // });
-  prisma = new PrismaClient();
+    prisma = new PrismaClient({
+      datasources: {
+        db: {
+          url: dbUrl[0] + "?sslcert=/tmp/DigiCertGlobalRootCA.crt.pem",
+        },
+      },
+    });
+  } else {
+    prisma = new PrismaClient();
+  }
 } else {
   if (!global.__db__) {
     global.__db__ = new PrismaClient();
