d44433212b23095f7a3cd478e9c5b36b8bd6eaae
luca_imdex <luca.marangon@imdexlimited.com>
2023-05-23 11:20:54 +0800

    feat: remove dev slot

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -1,19 +1,4 @@
 // learn more: https://fly.io/docs/reference/configuration/#services-http_checks
-import { prisma } from "~/db.server";
-import { trackException } from "~/services";
-
 export async function loader() {
-  try {
-    await prisma.chapter.count();
-
-    return new Response("OK");
-  } catch (error: unknown) {
-    console.error("healthcheck ❌", { error });
-
-    trackException({
-      exception: error as Error,
-    });
-
-    return new Response("ERROR", { status: 500 });
-  }
+  return new Response("OK");
 }
