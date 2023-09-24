8594d81518cc49c9cf390c4d5300fde5622dd3e9
Luca <duvet86@gmail.com>
2023-09-24 13:09:59 +0800

    feat: import students
    Signed-off-by: Luca <duvet86@gmail.com>

--- app/routes/_admin.students/components/ActionsDropdown.tsx
+++ app/routes/_admin.students/components/ActionsDropdown.tsx
@@ -4,14 +4,14 @@ import { DatabaseExport, DatabaseRestore, NavArrowDown } from "iconoir-react";
 
 export default function ActionsDropdown() {
   return (
-    <div className="dropdown dropdown-end">
+    <div className="dropdown-end dropdown">
       <label title="actions" tabIndex={0} className="btn w-40 gap-2">
         Actions
         <NavArrowDown className="h-6 w-6" />
       </label>
       <ul
         tabIndex={0}
-        className="menu dropdown-content rounded-box w-52 border border-base-300 bg-base-100 p-2 shadow"
+        className="dropdown-content menu rounded-box w-52 border border-base-300 bg-base-100 p-2 shadow"
       >
         <li>
           <Link className="gap-4" to="import">
