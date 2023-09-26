598cd605266c3c0a3577cbb28cf34b126ee65c97
Luca <duvet86@gmail.com>
2023-09-26 18:35:29 +0800

    feat: imported student history page
    Signed-off-by: Luca <duvet86@gmail.com>

---
+++ app/routes/admin.students_.import-history/services.server.ts
@@ -0,0 +1,29 @@
+import { prisma } from "~/db.server";
+
+export async function getImportHistoryAsync(
+  pageNumber: number,
+  numberItems = 10,
+) {
+  return await prisma.importedStudentHistory.findMany({
+    select: {
+      error: true,
+      createdAt: true,
+      student: {
+        select: {
+          id: true,
+          firstName: true,
+          lastName: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    skip: numberItems * pageNumber,
+    take: numberItems,
+  });
+}
+
+export async function getImportHistoryCountAsync() {
+  return await prisma.importedStudentHistory.count();
+}
