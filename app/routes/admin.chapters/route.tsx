dcda87d172f368d0fcdd4d88d2c55453f222f80f
Luca <duvet86@gmail.com>
2024-04-21 18:34:44 +0800

    feat: new chapters page

--- app/routes/admin.chapters/route.tsx
+++ app/routes/admin.chapters/route.tsx
@@ -20,66 +20,39 @@ export default function Index() {
     <>
       <Title>Chapters</Title>
 
-      <div className="overflow-auto bg-white">
-        <table className="table">
-          <thead>
-            <tr>
-              <th align="left">Name</th>
-              <th align="left" className="hidden lg:table-cell">
-                Address
-              </th>
-              <th align="right">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {chapters.map(({ id, name, address }) => (
-              <tr key={id}>
-                <td className="border">{name}</td>
-                <td className="hidden border lg:table-cell">{address}</td>
-                <td className="border">
-                  <div className="join h-12 w-full">
-                    <Link
-                      to={`${id}/roster`}
-                      className="btn btn-info join-item btn-xs h-full w-1/5 gap-2"
-                    >
-                      <Calendar className="h-4 w-4" />
-                      Roster
-                    </Link>
-                    <Link
-                      to={`${id}/students`}
-                      className="btn btn-primary join-item btn-xs h-full w-1/5 gap-2"
-                    >
-                      <GraduationCap className="h-4 w-4" />
-                      Students
-                    </Link>
-                    <Link
-                      to={`${id}/mentors`}
-                      className="btn btn-warning join-item btn-xs h-full w-1/5 gap-2"
-                    >
-                      <Group className="h-4 w-4" />
-                      Mentors
-                    </Link>
-                    <Link
-                      to={`${id}/reports`}
-                      className="btn join-item btn-xs h-full w-1/5 gap-2"
-                    >
-                      <PageEdit className="h-4 w-4" />
-                      Reports
-                    </Link>
-                    <Link
-                      to={id.toString()}
-                      className="btn btn-success join-item btn-xs h-full w-1/5 gap-2"
-                    >
-                      <PageEdit className="h-4 w-4" />
-                      Edit
-                    </Link>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      {chapters.map(({ id, name, address }) => (
+        <div key={id} className="card glass m-6 items-center">
+          <div className="card-body items-center text-center">
+            <h2 className="card-title uppercase">{name}</h2>
+            <p className="mb-6">{address}</p>
+            <div className="card-actions">
+              <Link to={`${id}/roster`} className="btn btn-info w-32 gap-2">
+                <Calendar className="h-4 w-4" />
+                Roster
+              </Link>
+              <Link
+                to={`${id}/students`}
+                className="btn btn-primary w-32 gap-2"
+              >
+                <GraduationCap className="h-4 w-4" />
+                Students
+              </Link>
+              <Link to={`${id}/mentors`} className="btn btn-warning w-32 gap-2">
+                <Group className="h-4 w-4" />
+                Mentors
+              </Link>
+              <Link to={`${id}/reports`} className="btn w-32 gap-2">
+                <PageEdit className="h-4 w-4" />
+                Reports
+              </Link>
+              <Link to={id.toString()} className="btn btn-success w-32 gap-2">
+                <PageEdit className="h-4 w-4" />
+                Edit
+              </Link>
+            </div>
+          </div>
+        </div>
+      ))}
     </>
   );
 }
