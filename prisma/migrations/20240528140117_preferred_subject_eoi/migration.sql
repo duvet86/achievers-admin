989137bca0d591d56cde9f1a1fe0df4e4e199d95
Luca <duvet86@gmail.com>
2024-05-29 19:54:45 +0800

    feat: preferred subject eoi

--- prisma/migrations/20240528140117_preferred_subject_eoi/migration.sql
+++ prisma/migrations/20240528140117_preferred_subject_eoi/migration.sql
@@ -1,2 +1,2 @@
 -- AlterTable
-ALTER TABLE `eoiprofile` ADD COLUMN `preferredSubject` VARCHAR(191) NOT NULL DEFAULT 'N/A';
+ALTER TABLE `EoIProfile` ADD COLUMN `preferredSubject` VARCHAR(191) NOT NULL DEFAULT 'N/A';
