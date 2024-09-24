e64a465ff76c6de3461b807c3cdf7af50dc7913c
Luca <duvet86@gmail.com>
2024-09-24 12:06:02 +0800

    feat: new roster

--- prisma/migrations/20240921093225_session_student_nullable/migration.sql
+++ prisma/migrations/20240921093225_session_student_nullable/migration.sql
@@ -1,8 +1,8 @@
 -- DropForeignKey
-ALTER TABLE `mentortostudentsession` DROP FOREIGN KEY `MentorToStudentSession_studentId_fkey`;
+ALTER TABLE `MentorToStudentSession` DROP FOREIGN KEY `MentorToStudentSession_studentId_fkey`;
 
 -- AlterTable
-ALTER TABLE `mentortostudentsession` MODIFY `studentId` INTEGER NULL;
+ALTER TABLE `MentorToStudentSession` MODIFY `studentId` INTEGER NULL;
 
 -- AddForeignKey
 ALTER TABLE `MentorToStudentSession` ADD CONSTRAINT `MentorToStudentSession_studentId_fkey` FOREIGN KEY (`studentId`) REFERENCES `Student`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
