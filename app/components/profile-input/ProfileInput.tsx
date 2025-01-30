d3e387cba3b5c3f1de2433d97501fedfcc60582e
Luca <duvet86@gmail.com>
2025-01-30 14:45:52 +0800

    fix: export mentor spreadsheet

--- app/components/profile-input/ProfileInput.tsx
+++ app/components/profile-input/ProfileInput.tsx
@@ -16,6 +16,30 @@ export default function ProfileInput({ defaultValue }: Props) {
 
   const hasImage = selectedImage ?? profilePicturePath;
 
+  const removeProfilePic = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (inputRef.current) {
+      inputRef.current.files = null;
+      inputRef.current.value = "";
+    }
+    setSelectedImage(null);
+    setProfilePicturePath(null);
+  };
+
+  const addProfilePic = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0] ?? null;
+    if (
+      file &&
+      (file.type === "image/png" ||
+        file.type === "image/gif" ||
+        file.type === "image/jpeg")
+    ) {
+      setSelectedImage(file);
+    } else {
+      setSelectedImage(null);
+    }
+  };
+
   return (
     <div className="card card-side bg-base-100 m-8 shadow-xl">
       <figure>
@@ -35,15 +59,7 @@ export default function ProfileInput({ defaultValue }: Props) {
         {hasImage && (
           <button
             className="btn btn-error w-32 gap-2"
-            onClick={(e) => {
-              e.preventDefault();
-              if (inputRef.current) {
-                inputRef.current.files = null;
-                inputRef.current.value = "";
-              }
-              setSelectedImage(null);
-              setProfilePicturePath(null);
-            }}
+            onClick={removeProfilePic}
           >
             <Xmark className="h-6 w-6" />
             Remove
@@ -56,9 +72,7 @@ export default function ProfileInput({ defaultValue }: Props) {
           className={hasImage ? "hidden" : "file-input w-full max-w-xs"}
           ref={inputRef}
           accept="image/png, image/gif, image/jpeg"
-          onChange={(event) => {
-            setSelectedImage(event.target.files?.[0] ?? null);
-          }}
+          onChange={addProfilePic}
         />
 
         <input
