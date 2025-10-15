e72ba82b2c12fe0489ce4be9cd44d85085058716
Luca <duvet86@gmail.com>
2025-10-15 13:46:59 +0800

    fix: grades unique constraint

--- prisma/migrations/20251015042534_fix_grades_unique_constraint/migration.sql
+++ prisma/migrations/20251015042534_fix_grades_unique_constraint/migration.sql
@@ -1,5 +1,5 @@
 -- DropIndex
-DROP INDEX `StudentGrade_year_semester_subject_key` ON `studentgrade`;
+DROP INDEX `StudentGrade_year_semester_subject_key` ON `StudentGrade`;
 
 -- CreateIndex
 CREATE UNIQUE INDEX `StudentGrade_year_semester_subject_studentId_key` ON `StudentGrade`(`year`, `semester`, `subject`, `studentId`);
