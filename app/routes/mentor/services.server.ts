49cacb61235a4e8789efd4b9d2a688b53cfd1386
Luca <duvet86@gmail.com>
2025-11-26 19:57:57 +0800

    fix: archived mentor

--- app/routes/mentor/services.server.ts
+++ app/routes/mentor/services.server.ts
@@ -4,6 +4,7 @@ export async function getUserByAzureADIdAsync(azureADId: string) {
   return await prisma.mentor.findUnique({
     where: {
       azureADId,
+      endDate: null,
     },
     select: {
       volunteerAgreementSignedOn: true,
